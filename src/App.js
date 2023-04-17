431cdcda803c9dc7d8d5507cccc1aaf980f02fbf
mrtechnostart <rambpandey238@gmail.com>
2023-04-18 02:21:29 +0530

    Fix Overlapping && WhyBlaze Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Components/Home';
+import WhyBlaze from './Components/WhyBlaze';
 function App() {
   return (
     <>
       <Home/>
+      <WhyBlaze/>
     </>
   );
 }

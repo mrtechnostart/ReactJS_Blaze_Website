18474592f4921fb33204f180f87cfd5cd3b23df6
mrtechnostart <rambpandey238@gmail.com>
2023-04-18 02:04:13 +0530

    Completed Home Component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import "./Stylesheet/Home.css";
 
 const Home = () => {
   return (
-    <>
-    <Navbar/>
-    </>
-  )
-}
+    <div className="master">
+        <Navbar />
+      <div className="father d-flex flex-column flex-md-row container justify-content-between align-items-center my-5">
+        <div className="child-1 order-2 order-md-1">
+          <h2>
+            Introducing
+            <br />
+            Project Blaze
+          </h2>
+          <h3 className="headerh3">An AOSP Based Operating System</h3>
+          <p>All your needs are satisfied here!</p>
+          <a href="download.html" style={{ textDecoration: "none" }}>
+            <button type="button" className="downloadBtn">
+              Download Now <img src={"images/arrow.png"} alt="arrow"/>
+            </button>
+          </a>
+        </div>
+        <div className="child-2 text-center my-5 my-md-0 order-md-2 order-1">
+          <img src={"images/device.png"} alt="Device" className="controller" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
